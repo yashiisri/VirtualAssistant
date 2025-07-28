e8cee11ca3901ebced8dd3d6ea2c426f81ab6d8a
Yashi Srivastava <139585327+yashiisri@users.noreply.github.com>
2025-07-28 23:33:41 +0530

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import bg from "../assets/authBg.png";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 import axios from "axios";
 
 function SignIn() {
