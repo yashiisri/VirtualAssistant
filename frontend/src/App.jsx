430fe12a6681e5e370f07974a1def19f43040aad
Yashi Srivastava <139585327+yashiisri@users.noreply.github.com>
2025-07-28 23:33:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,3 @@
-// import React, { useContext } from 'react'
-// import { Navigate, Route, Routes } from 'react-router-dom'
-// import SignUp from './pages/SignUp'
-// import SignIn from './pages/SignIn'
-// import Customize from './pages/Customize'
-// import { userDataContext } from './context/userContext'
-// import Home from './pages/Home'
-// import Customize2 from './pages/Customize2.jsx'
-
-// function App() {
-//   const {userdata}=useContext(userDataContext)
-//   return (
-//   <Routes>
-    
-//       <Route path="/" element={(userdata?.assistantImage && userdata?.assistantName)?<Home/>:<Navigate to={"/customize"}/>}/>
-      
-//     <Route path='/signup' element={!userdata?<SignUp/>:<Navigate to={"/customize"}/>}/>
-//     <Route path='/login' element={!userdata?<SignIn/>:<Navigate to={"/"}/>}/>
-//      <Route path='/customize' element={userdata?<Customize/>:<Navigate to={"/signup"}/>}/>
-//           <Route path='/customize2' element={userdata?<Customize2/>:<Navigate to={"/signup"}/>}/>
-//   </Routes>
-//   )
-// }
-
-// export default App
-
 
 
 
@@ -34,7 +8,7 @@ import SignIn from './pages/SignIn';
 import Customize from './pages/Customize';
 import Customize2 from './pages/Customize2';
 import Home from './pages/Home';
-import { userDataContext } from './context/userContext';
+import { userDataContext } from './context/UserContext';
 
 function App() {
   const { userdata } = useContext(userDataContext);
